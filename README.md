fcbec2dbdc839f5d6cc09a0f7ce807fe0da9f96d
Ruslan Krokhin <ulitcos@gmail.com>
2021-01-06 21:29:54 +0300

    build with new grep

--- README.md
+++ README.md
@@ -1,22 +1,31 @@
 # object-grep-chrome-extension
-A Chrome extension that adds the grep method to the object prototype. Based on [object-grep](https://github.com/ulitcos/object-grep) tool. 
+
+A Chrome extension that adds the grep method to the object prototype. Based
+on [object-grep](https://github.com/ulitcos/object-grep) tool.
 
 ## install
+
 Сlone to your machine:
+
 ```bash
 git clone https://github.com/ulitcos/object-grep-chrome-extension.git
 ```
 
 Install extension into chrome ([source](https://developer.chrome.com/extensions/getstarted#manifest)):
+
 - Open the Extension Management page by navigating to chrome://extensions.
 - Enable Developer Mode by clicking the toggle switch next to Developer mode.
 - Click the LOAD UNPACKED button and select the extension directory.
 
 ## usage
-The grep method will be embedded in `object.prototype`, so you can call it on any object
-```javascript
-const obj = { a: { b : { c: 'd' } } }
 
-obj.grep('b') // => {keys: ['a.b']}
+The grep method will be embedded in `object.prototype`, so you can call it on any object
+
+```javascript
+const obj = {a: {b: {c: 'd'}}}
+
+obj.grep('b') // => {inKeys: {'a.b': {c: 'd'}}, inValues: {}}
 ```
-More examples [here](https://github.com/ulitcos/object-grep#usage) and [here](https://github.com/ulitcos/object-grep/blob/master/tests/test.js)
+
+More examples [here](https://github.com/ulitcos/object-grep#usage)
+and [here](https://github.com/ulitcos/object-grep/blob/master/tests/test.js)
--- lib.js
+++ lib.js
@@ -1,44 +1,68 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
-// CONCATENATED MODULE: ./node_modules/object-grep/src/index.js
-function objectGrep(obj, searchExpr, depth = 100, path = '', result = {}) {
-  if (depth === 0) {
+;// CONCATENATED MODULE: ./node_modules/object-grep/src/index.js
+function grep({target, searchExpr, depth, path, result}) {
+  if (depth < 1) {
     return result;
   }
 
-  if (!searchExpr) {
-    searchExpr = String(searchExpr);
-  }
-
-  Object.keys(obj).forEach((key) => {
+  Object.keys(target).forEach((key) => {
     const keyPath = !path ? key : `${path}.${key}`;
-    const value = obj[key];
+    const value = target[key];
 
     if (key.match(searchExpr)) {
-      if (!result.keys) {
-        result.keys = [];
-      }
-
-      result.keys.push(keyPath);
+      result.inKeys[keyPath] = value;
     }
 
     if (['string', 'number', 'boolean', 'undefined'].includes(typeof value) || value === null) {
       if (String(value).match(searchExpr)) {
-        if (!result.values) {
-          result.values = [];
-        }
-
-        result.values.push(keyPath);
+        result.inValues[keyPath] = value;
       }
 
       return;
     }
 
-    objectGrep(value, searchExpr, depth - 1, keyPath, result);
+    grep({
+      searchExpr,
+      result,
+      path: keyPath,
+      target: value,
+      depth: depth - 1,
+    });
   });
 
   return result;
+}
+
+function objectGrep(target, searchExpr, depth = 20) {
+  const result = Object.create({
+      short: function () {
+        return {
+          inKeys: Object.keys(this.inKeys),
+          inValues: Object.keys(this.inValues),
+        };
+      }
+    },
+    {
+      inValues: {writable: true, configurable: true, enumerable: true, value: {}},
+      inKeys: {writable: true, configurable: true, enumerable: true, value: {}}
+    }
+  );
+
+  const path = '';
+
+  if (!searchExpr) {
+    searchExpr = String(searchExpr);
+  }
+
+  return grep({
+    target,
+    searchExpr,
+    depth,
+    path,
+    result
+  });
 }
 
 objectGrep.inject = function () {
@@ -50,8 +74,8 @@ objectGrep.inject = function () {
   });
 };
 
-// CONCATENATED MODULE: ./src/lib.js
-;
+;// CONCATENATED MODULE: ./src/lib.js
+
 
 objectGrep.inject();
 
