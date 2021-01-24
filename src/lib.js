3dcffdf455fab9f6f6134b3f4dc747f4565cae77
Ruslan Krokhin <ulitcos@gmail.com>
2021-01-25 00:33:28 +0300

    build with new object-grep

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,9 @@
 import { objectGrep } from 'object-grep';
 
-try {
-  objectGrep.inject();
-} catch(e) {
-  console.error('[Object grep error]: Object.prototype already has method grep. Injection failed.')
-}
-
+window.addEventListener("load", () => {
+  try {
+    objectGrep.inject('__grep__');
+  } catch(e) {
+    console.error('[Object grep error]: Object.prototype already has method grep. Injection failed.')
+  }
+});
